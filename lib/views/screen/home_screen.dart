5f0bd6747bead09fb7d4e22883eae1f450e58e87
ASLIDDINCODER <147050428+ASLIDDINCODERn1@users.noreply.github.com>
2024-06-04 10:50:21 +0500

    uptdate new navigation bar

--- lib/views/screen/home_screen.dart
+++ lib/views/screen/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:perfect_settings_ui/views/screen/notes_screen.dart';
+import 'package:perfect_settings_ui/views/screen/todo_screen.dart';
 import 'package:perfect_settings_ui/views/widgets/customized_drawer.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -17,7 +19,20 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Color _appBarColor = Colors.blue;
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
+  List<Widget> screens = [
+    const Center(child: Text('HOME SCREEN')),
+    const Center(child: NoteScreen()),
+    const Center(
+      child: TodoScreen(),
+    ),
+  ];
   @override
   void initState() {
     super.initState();
@@ -77,24 +92,54 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: _appBarColor,
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.color_lens),
-            onPressed: _showColorPickerDialog,
-          ),
-        ],
-      ),
-      drawer: CustomizedDrawer(
-        onThemeModeChanged: widget.onThemeModeChanged,
-        onColorChanged: _onColorChanged,
-      ),
-      body: const Center(
-        child: Text('Home Screen Content'),
-      ),
-    );
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.notes), label: 'Notes'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.today_outlined), label: 'Todo'),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onItemTapped,
+        ),
+        appBar: AppBar(
+          title: const Text('Home'),
+          backgroundColor: _appBarColor,
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.color_lens),
+              onPressed: _showColorPickerDialog,
+            ),
+          ],
+        ),
+        drawer: CustomizedDrawer(
+          onThemeModeChanged: widget.onThemeModeChanged,
+          onColorChanged: _onColorChanged,
+        ),
+        body: Row(
+          children: <Widget>[
+            NavigationRail(
+              backgroundColor: Colors.amber,
+              onDestinationSelected: _onItemTapped,
+              selectedIndex: _selectedIndex,
+              labelType: NavigationRailLabelType.all,
+              destinations: const <NavigationRailDestination>[
+                NavigationRailDestination(
+                    icon: Icon(Icons.home), label: Text('Home')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.note_alt_sharp), label: Text('Notes')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.today_rounded), label: Text('Todo')),
+              ],
+            ),
+            Expanded(
+                child: IndexedStack(
+              index: _selectedIndex,
+              children: screens,
+            ))
+          ],
+        ));
   }
 }
